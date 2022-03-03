20e9b52791950c6bf296d0e2980e941aa8a206d0
Kesavan Yogeswaran <hikes@google.com>
2022-03-03 00:47:25 -0500

    Simplify logic

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Enumerates all executables in `PATH`. UNIX-only.
 
 Some use cases:
+
 * Pipe to fzf → Launch stuff from your terminal → profit
 * Much faster [`dmenu_path`](https://git.suckless.org/dmenu/file/dmenu_path.html) replacement
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,33 @@
-/// Enumerate all executables on PATH
+/// # pathsearch 🕵
+///
+/// Enumerate all executables in `$PATH`. Unix-only.
 ///
 /// Potential use cases:
-/// * Pipe output to fzf and have an executable runner
-/// * dmenu_path replacement
+///
+/// * Pipe to fzf → Launch stuff from your terminal → profit
+/// * Faster dmenu_path replacement
 use anyhow::{anyhow, Result};
+use nix::unistd::{self, AccessFlags};
 use rayon::prelude::*;
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 use std::ffi::OsString;
-use std::os::unix::prelude::*;
+use std::fs::DirEntry;
 use std::path::{Path, PathBuf};
 use std::{env, fs};
 
-fn get_executables<T: AsRef<Path>>(path: T) -> Result<Vec<OsString>> {
-    // TODO: check if this handles symbolic links properly
-    // TODO: use jwalk crate for better parallelism
+fn is_executable(entry: DirEntry) -> Option<OsString> {
+    if !entry.file_type().ok()?.is_dir() && unistd::access(&entry.path(), AccessFlags::X_OK).is_ok()
+    {
+        Some(entry.file_name())
+    } else {
+        None
+    }
+}
+
+fn find_executables<T: AsRef<Path>>(path: T) -> Result<Vec<OsString>> {
     let entries = fs::read_dir(&path)?
-        .filter_map(|entry| match entry {
-            Ok(entry) => {
-                if entry.file_type().unwrap().is_file()
-                    // Check if the owner has exec permission
-                    // TODO: check if current user has exec permission
-                    // TODO: check if this is reason for difference vs. dmenu_path
-                    && entry.metadata().unwrap().mode() & 0o200 != 0
-                {
-                    Some(entry.file_name())
-                } else {
-                    None
-                }
-            }
-            Err(_) => None,
-        })
+        .flatten()
+        .filter_map(is_executable)
         .collect();
     Ok(entries)
 }
@@ -37,15 +35,12 @@ fn get_executables<T: AsRef<Path>>(path: T) -> Result<Vec<OsString>> {
 fn main() -> Result<()> {
     let path = env::var_os("PATH").ok_or_else(|| anyhow!("PATH not set"))?;
     let paths: Vec<PathBuf> = env::split_paths(&path).collect();
-    let mut cmds: Vec<OsString> = paths
+    // Use BTreeSet for deduping + sorting
+    let cmds: BTreeSet<OsString> = paths
         .into_par_iter()
-        .flat_map(get_executables)
+        .flat_map(find_executables)
         .flatten()
-        // Dedupe
-        .collect::<HashSet<OsString>>()
-        .drain()
-        .collect::<Vec<OsString>>();
-    cmds.par_sort_unstable();
+        .collect();
 
     for cmd in cmds {
         if let Some(cmd) = cmd.to_str() {
